14ba99d4531ca90b05698bc69f7ae1cad1a473ee
Adarsh P S <adarshps@Adarshs-MacBook-Air.local>
2025-05-22 15:48:48 +0530

    changed from dart ui to dart ui web

--- lib/web/register_view_factory.dart
+++ lib/web/register_view_factory.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 import 'dart:js_interop';
-import 'dart:ui' as ui;
+import 'dart:ui_web' as ui;
 import 'package:web/web.dart' as web;
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:video_js_themed/src/view_factory_plugin.dart';
